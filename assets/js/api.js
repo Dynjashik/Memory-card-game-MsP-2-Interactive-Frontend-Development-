2ec6e41c813b7bbdc2b27c8b00b734e096804eda
Dynjashik <stasenko.dasha.s@gmail.com>
2021-03-23 12:29:39 +0000

    fix html,css,js validators errors

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,7 +3,7 @@ const baseURL = "https://dog.ceo/api/breeds/image/random/";
 function getCards(cardAmount, writeCards) {
     var xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             writeCards(JSON.parse(this.responseText));
         }
@@ -14,56 +14,55 @@ function getCards(cardAmount, writeCards) {
 }
 
 
-
 function showCards(cardAmount) {
-    console.log("show cards")
+    console.log("show cards");
     var el = document.getElementById("cards");
     el.innerHTML = "";
-    var imagesArray = []
+    var imagesArray = [];
 
-    getCards(cardAmount, function(data) {
+    getCards(cardAmount, function (data) {
         data = data.message;
-        
-        var backCard = "<div class = \"back-side card-side\"><img src=\"assets/images/dog-logo-black.png\" width=\"150\", height=\"150\"></div>"
 
-        data.forEach(function(item) {
-            var frontCard = "<div class = \"front-side card-side \" ><img src=\""+ item + "\" width=\"150\", height=\"150\"></div>"
-            imagesArray.push(frontCard)
-            imagesArray.push(frontCard)
+        var backCard = "<div class = \"back-side card-side\"><img src=\"assets/images/dog-logo-black.png\" width=\"150\", height=\"150\"></div>";
+
+        data.forEach(function (item) {
+            var frontCard = "<div class = \"front-side card-side \" ><img src=\"" + item + "\" width=\"150\", height=\"150\"></div>";
+            imagesArray.push(frontCard);
+            imagesArray.push(frontCard);
         });
 
-        imagesArray = shuffle(imagesArray)
+        imagesArray = shuffle(imagesArray);
 
-        var row = ""
+        var row = "";
         for (i in imagesArray) {
-           row += backCard + imagesArray[i]
-           var row_counter = parseInt(i)+1
-           if (row_counter%4 == 0) {
-                el.innerHTML += "<div>" + row + "</div>"
-                row = ""
+            row += backCard + imagesArray[i];
+            var row_counter = parseInt(i) + 1;
+            if (row_counter % 4 == 0) {
+                el.innerHTML += "<div>" + row + "</div>";
+                row = "";
             }
         }
 
-    }); 
+    });
 }
 
 /* Function Shuffle the cards from external source: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array*/
 
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-  return array;
+    return array;
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,101 +1,100 @@
+var openedCardCount = 0;
+var openedCards = [];
+var amountFlips = 0;
+var amount = 0;
 
-var openedCardCount = 0
-var openedCards = []
-var amountFlips = 0
-var amount = 0
-
-const steps = document.getElementById('steps')
+var steps = document.getElementById('steps');
 
 /*Clicking level buttons*/
 
-$(".btn-level").click(function(){
-    console.log("gamewindow")
-    $(".menu-page").removeClass("d-block")
-    $(".menu-page").addClass("d-none")
-    $(".game-window").removeClass("d-none")
-    $(".game-window").addClass("d-block")
-    $("#playWindow").modal("hide")
-    amount = $(this).attr("difficulty")
-    showCards(amount)
-    amountFlips = 0
-    var nameLevel = $(this).text()
-    steps.innerHTML = " Steps: " + amountFlips
-    level.innerHTML = " Level: " + nameLevel
-})
+$(".btn-level").click(function () {
+    console.log("gamewindow");
+    $(".menu-page").removeClass("d-block");
+    $(".menu-page").addClass("d-none");
+    $(".game-window").removeClass("d-none");
+    $(".game-window").addClass("d-block");
+    $("#playWindow").modal("hide");
+    amount = $(this).attr("difficulty");
+    showCards(amount);
+    amountFlips = 0;
+    var nameLevel = $(this).text();
+    steps.innerHTML = " Steps: " + amountFlips;
+    level.innerHTML = " Level: " + nameLevel;
+});
 
-$(".btn-restart").click(function(){
-    console.log("restart")
-    $(".menu-page").removeClass("d-none")
-    $(".menu-page").addClass("d-block")
-    $(".game-window").removeClass("d-block")
-    $(".game-window").addClass("d-none")
-    $("#gameEnd").modal("hide")
-    $("#playWindow").modal("show")
-    timer.innerHTML = "Time: 00:00"
-    stopTimer()
-})
+$(".btn-restart").click(function () {
+    console.log("restart");
+    $(".menu-page").removeClass("d-none");
+    $(".menu-page").addClass("d-block");
+    $(".game-window").removeClass("d-block");
+    $(".game-window").addClass("d-none");
+    $("#gameEnd").modal("hide");
+    $("#playWindow").modal("show");
+    timer.innerHTML = "Time: 00:00";
+    stopTimer();
+});
 
-$(".btn-mainmenu").click(function(){
-    console.log("mainmenu")
-    $(".menu-page").removeClass("d-none")
-    $(".menu-page").addClass("d-block")
-    $(".game-window").removeClass("d-block")
-    $(".game-window").addClass("d-none")
-    $("#gameEnd").modal("hide")
-    timer.innerHTML = "Time: 00:00"
-    stopTimer()
-})
+$(".btn-mainmenu").click(function () {
+    console.log("mainmenu");
+    $(".menu-page").removeClass("d-none");
+    $(".menu-page").addClass("d-block");
+    $(".game-window").removeClass("d-block");
+    $(".game-window").addClass("d-none");
+    $("#gameEnd").modal("hide");
+    timer.innerHTML = "Time: 00:00";
+    stopTimer();
+});
 
 /*Flipping game cards, flips amounter*/
 
-$(".card").on('click','.back-side', function() {
-    if (amountFlips==0) {
-        startTimer()
+$(".card").on('click', '.back-side', function () {
+    if (amountFlips == 0) {
+        startTimer();
     }
-    if (openedCardCount<2) {
+    if (openedCardCount < 2) {
         $(this).hide();
         $(this).next(".front-side").css("display", "inline-block").addClass("visible");
-        openedCardCount=openedCardCount+1
-        amountFlips = amountFlips+1
-        steps.innerHTML = " Steps: " + amountFlips 
+        openedCardCount = openedCardCount + 1;
+        amountFlips = amountFlips + 1;
+        steps.innerHTML = " Steps: " + amountFlips;
 
-        selectedImageURL = $(this).next(".front-side").find("img").attr("src")
-        openedCards.push(selectedImageURL)
+        selectedImageURL = $(this).next(".front-side").find("img").attr("src");
+        openedCards.push(selectedImageURL);
 
-        console.log(openedCards,openedCardCount)
+        console.log(openedCards, openedCardCount);
 
         if (openedCardCount == 2) {
-            if(openedCards[0] != openedCards[1]) {
-               setTimeout(function () {
-                $(".back-side").show();
-                $(".front-side").not('.matched').hide().removeClass("visible");
-                console.log("COMPARE CARDS")
-                openedCardCount = 0
+            if (openedCards[0] != openedCards[1]) {
+                setTimeout(function () {
+                    $(".back-side").show();
+                    $(".front-side").not('.matched').hide().removeClass("visible");
+                    console.log("COMPARE CARDS");
+                    openedCardCount = 0;
                 }, 1000);
-            }  else if (openedCards[0] == openedCards[1]) {
-               $(".visible").addClass("matched")
-               $(".matched").prev(".back-side").removeClass("back-side")
-                console.log("matchedCards")
-                openedCardCount = 0
-               
-/*Openning modal window with game results*/
+            } else if (openedCards[0] == openedCards[1]) {
+                $(".visible").addClass("matched");
+                $(".matched").prev(".back-side").removeClass("back-side");
+                console.log("matchedCards");
+                openedCardCount = 0;
 
-                  var numItems = $('.matched').length
-                  if (numItems == amount*2) {
-                      console.log("endGame")
-                      $("#gameEnd").modal("show")
-                      stopTimer()
-                  }
-                  console.log("numItems: "+numItems + "  amount: " + amount)
-                  if (finalMin<1){
-                      timeResult.innerHTML = finalSec + " seconds ";
-                  } else {
-                      timeResult.innerHTML = finalMin + " minutes " + finalSec + " seconds ";
-                  }
-                  stepsResult.innerHTML = amountFlips + " steps";
-            
+                /*Openning modal window with game results*/
+
+                var numItems = $('.matched').length;
+                if (numItems == amount * 2) {
+                    console.log("endGame");
+                    $("#gameEnd").modal("show");
+                    stopTimer();
+                }
+                console.log("numItems: " + numItems + "  amount: " + amount);
+                if (finalMin < 1) {
+                    timeResult.innerHTML = finalSec + " seconds ";
+                } else {
+                    timeResult.innerHTML = finalMin + " minutes " + finalSec + " seconds ";
+                }
+                stepsResult.innerHTML = amountFlips + " steps";
+
             }
-            openedCards = []
+            openedCards = [];
         }
     }
 
@@ -112,20 +111,20 @@ var finalMin;
 var stoptime = true;
 
 function startTimer() {
-  if (stoptime == true) {
-      stoptime = false;
-      timerCycle();
+    if (stoptime == true) {
+        stoptime = false;
+        timerCycle();
     }
 }
 
 function stopTimer() {
-  if (stoptime == false) {
-      stoptime = true;
-      finalSec = sec
-      finalMin = min
-      sec = 0;
-      min = 0;
-  }
+    if (stoptime == false) {
+        stoptime = true;
+        finalSec = sec;
+        finalMin = min;
+        sec = 0;
+        min = 0;
+    }
 }
 
 function timerCycle() {
@@ -133,23 +132,22 @@ function timerCycle() {
         sec = parseInt(sec);
         min = parseInt(min);
         sec = sec + 1;
-        console.log("cycle")
+        console.log("cycle");
 
-    if (sec == 60) {
-        min = min + 1;
-        sec = 0;
-    }
-    
-    if (sec < 10 || sec == 0) {
-        sec = '0' + sec;
-    }
-    if (min < 10 || min == 0) {
-        min = '0' + min;
-    }
+        if (sec == 60) {
+            min = min + 1;
+            sec = 0;
+        }
 
-    timer.innerHTML = "Time: "  + min + ':' + sec;
+        if (sec < 10 || sec == 0) {
+            sec = '0' + sec;
+        }
+        if (min < 10 || min == 0) {
+            min = '0' + min;
+        }
 
-    setTimeout("timerCycle()", 1000);
-  }
+        timer.innerHTML = "Time: " + min + ':' + sec;
+
+        setTimeout("timerCycle()", 1000);
+    }
 }
-
